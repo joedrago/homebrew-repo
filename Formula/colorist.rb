2aadceabe7ae29951fedda3a3ef558e28335ed5f
Joe Drago <joedrago@gmail.com>
2019-06-11 16:25:18 -0700

    colorist v0.0.44

--- Formula/colorist.rb
+++ Formula/colorist.rb
@@ -1,15 +1,15 @@
 class Colorist < Formula
   desc "Image Converter with heavy ICC lumi tag abuse"
   homepage "https://github.com/joedrago/colorist"
-  url "https://github.com/joedrago/colorist/archive/v0.0.43.tar.gz"
-  sha256 "d6cd720a5ef9dde7f8eeed2776334aa6c8b04ceb108b8af7898c65cea90b1c84"
+  url "https://github.com/joedrago/colorist/archive/v0.0.44.tar.gz"
+  sha256 "75665b483f566472163893f3d8f12940fcf917deffec19b83fb8a0598d932204"
   depends_on "cmake" => :build
   depends_on "nasm" => :build
 
   def install
     mkdir_p "build"
 	cd "build"
-    system "cmake", "-DHOMEBREW_BUILD=1", "..", *std_cmake_args
+    system "cmake", "-DHOMEBREW_BUILD=1", "-DCMAKE_BUILD_TYPE=Release", "..", *std_cmake_args
     system "make", "colorist-bin"
     bin.install "bin/colorist/colorist"
   end
