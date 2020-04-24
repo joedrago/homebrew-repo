6b30ea62a19459e4893681bcfdffadeee9cd2748
Joe Drago <jdrago@netflix.com>
2020-04-24 13:29:43 -0700

    colorist v0.1.14 (pre-bottle)

--- Formula/colorist.rb
+++ Formula/colorist.rb
@@ -1,8 +1,8 @@
 class Colorist < Formula
   desc "Image Converter with heavy ICC lumi tag abuse"
   homepage "https://github.com/joedrago/colorist"
-  url "https://github.com/joedrago/colorist/archive/v0.1.10.tar.gz"
-  sha256 "d5d599c85651ab6da96e592238e40db5b5dee1d84617740d1b0da942c97fcd1b"
+  url "https://github.com/joedrago/colorist/archive/v0.1.14.tar.gz"
+  sha256 "ef47248d8616b08c13d4f5af26f790df0721c74b5c99aae8e907aca9c6b4e4f8"
   depends_on "cmake" => :build
   depends_on "nasm" => :build
   depends_on "rust" => :build
@@ -19,11 +19,11 @@ class Colorist < Formula
     bin.install "#{buildpath}/build/bin/colorist/colorist"
   end
 
-  bottle do
-    root_url "https://github.com/joedrago/homebrew-repo/releases/download/colorist-v0.1.10/"
-    cellar :any_skip_relocation
-    sha256 "fbe0e94d6d5efaa4e5e06052f88a7fca10ecd34e886218f6b90cb76998639393" => :catalina
-  end
+  #bottle do
+  #  root_url "https://github.com/joedrago/homebrew-repo/releases/download/colorist-v0.1.10/"
+  #  cellar :any_skip_relocation
+  #  sha256 "fbe0e94d6d5efaa4e5e06052f88a7fca10ecd34e886218f6b90cb76998639393" => :catalina
+  #end
 
   test do
     system "false"
