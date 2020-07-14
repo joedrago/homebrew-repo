54c6e670c5d8b6933e6c9774a832c875909fe7d8
Joe Drago <jdrago@netflix.com>
2020-07-14 13:07:40 -0700

    avifenc v0.8.0 (pre-bottle)

--- Formula/avifenc.rb
+++ Formula/avifenc.rb
@@ -1,8 +1,8 @@
 class Avifenc < Formula
   desc "avifenc / avifdec"
   homepage "https://github.com/AOMediaCodec/libavif"
-  url "https://github.com/AOMediaCodec/libavif/archive/v0.7.3.tar.gz"
-  sha256 "a4ce03649c58ec9f3dc6ab2b7cf7d58474b149acf1e4c563be4081bad60ed2dd"
+  url "https://github.com/AOMediaCodec/libavif/archive/v0.8.0.tar.gz"
+  sha256 "c917f4a27add61b8ad15f328090942d519460b71219f31e60e5fe6aa8cf3dc4e"
   depends_on "cmake" => :build
   depends_on "nasm" => :build
   depends_on "ninja" => :build
@@ -22,11 +22,11 @@ class Avifenc < Formula
     bin.install "#{buildpath}/build/avifdump"
   end
 
-  bottle do
-    root_url "https://github.com/joedrago/homebrew-repo/releases/download/avifenc-v0.7.3/"
-    cellar :any_skip_relocation
-    sha256 "f0000d9ebc6a9408c1aec6d30e8bf68dd28942e9929be3e6370c251041b3ac91" => :catalina
-  end
+  #bottle do
+  #  root_url "https://github.com/joedrago/homebrew-repo/releases/download/avifenc-v0.7.3/"
+  #  cellar :any_skip_relocation
+  #  sha256 "f0000d9ebc6a9408c1aec6d30e8bf68dd28942e9929be3e6370c251041b3ac91" => :catalina
+  #end
 
   test do
     system "false"
