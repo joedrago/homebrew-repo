1e398d9971625ec074f8584a4deed24d66decccd
Brian Gyss <brian.gyss@gmail.com>
2021-04-28 15:15:38 -0700

    fixed bottle block deprecations

--- Formula/avifenc.rb
+++ Formula/avifenc.rb
@@ -1,30 +1,30 @@
 class Avifenc < Formula
-  desc "avifenc / avifdec"
+  desc "/ avifdec"
   homepage "https://github.com/AOMediaCodec/libavif"
   url "https://github.com/AOMediaCodec/libavif/archive/v0.8.4.tar.gz"
   sha256 "116b46a9a497c6ef178c5a20b912d8ac02f888dc8c537e40b5d16700823a9b16"
+  bottle do
+    root_url "https://github.com/joedrago/homebrew-repo/releases/download/avifenc-v0.8.4/"
+    sha256 cellar: :any_skip_relocation, catalina: "321f5bce8acd64c54ee29cbf7e30b4fd70707970bb6072681a5fc5ea23e12177"
+  end
+
   depends_on "cmake" => :build
   depends_on "nasm" => :build
   depends_on "ninja" => :build
 
   def install
-    Dir.chdir('ext')
+    Dir.chdir("ext")
     system "zsh", "aom.cmd"
     system "zsh", "libjpeg.cmd"
     system "zsh", "zlibpng.cmd"
-    Dir.chdir('..')
-    Dir.mkdir('build')
-    Dir.chdir('build')
-    system "cmake", "-G", "Ninja", "-DBUILD_SHARED_LIBS=0", "-DAVIF_CODEC_AOM=1", "-DAVIF_LOCAL_AOM=1", "-DAVIF_LOCAL_ZLIBPNG=1", "-DAVIF_LOCAL_JPEG=1", "-DAVIF_BUILD_APPS=1", ".."
+    Dir.chdir("..")
+    Dir.mkdir("build")
+    Dir.chdir("build")
+    system "cmake", "-G", "Ninja", "-DBUILD_SHARED_LIBS=0", "-DAVIF_CODEC_AOM=1", "-DAVIF_LOCAL_AOM=1",
+"-DAVIF_LOCAL_ZLIBPNG=1", "-DAVIF_LOCAL_JPEG=1", "-DAVIF_BUILD_APPS=1", ".."
     system "ninja"
     bin.install "#{buildpath}/build/avifenc"
     bin.install "#{buildpath}/build/avifdec"
-  end
-
-  bottle do
-    root_url "https://github.com/joedrago/homebrew-repo/releases/download/avifenc-v0.8.4/"
-    cellar :any_skip_relocation
-    sha256 "321f5bce8acd64c54ee29cbf7e30b4fd70707970bb6072681a5fc5ea23e12177" => :catalina
   end
 
   test do
--- Formula/colorist.rb
+++ Formula/colorist.rb
@@ -3,11 +3,16 @@ class Colorist < Formula
   homepage "https://github.com/joedrago/colorist"
   url "https://github.com/joedrago/colorist/archive/v0.1.21.tar.gz"
   sha256 "3b04e4ddb44d97e64879672733f7285f1e603e7da10ce2a85142fc1dbad61c7d"
+  bottle do
+    root_url "https://github.com/joedrago/homebrew-repo/releases/download/colorist-v0.1.21/"
+    sha256 cellar: :any_skip_relocation, catalina: "a684ae81c634aba97be00b3ed563a6ab7f27b1843850f5f9ad63657b23dd6e90"
+  end
+
   depends_on "cmake" => :build
-  depends_on "nasm" => :build
-  depends_on "rust" => :build
   depends_on "meson" => :build
+  depends_on "nasm" => :build
   depends_on "ninja" => :build
+  depends_on "rust" => :build
 
   def install
     # Pre-install a local copy of cbindgen as rav1e.cmd won't find it in the PATH
@@ -15,17 +20,11 @@ class Colorist < Formula
     system "cargo", "install", "cbindgen", "--root", "#{buildpath}/cargoroot"
     ENV.append_path "PATH", "#{buildpath}/cargoroot/bin"
 
-    # Note: The branch here must match the branch used by the version of colorist above!
+    # NOTE: The branch here must match the branch used by the version of colorist above!
     system "git", "clone", "-b", "v0.8.3", "https://github.com/AOMediaCodec/libavif.git", "ext/libavif"
 
     system "zsh", "./scripts/build.sh", "-DHOMEBREW_BUILD=1"
     bin.install "#{buildpath}/build/bin/colorist/colorist"
-  end
-
-  bottle do
-    root_url "https://github.com/joedrago/homebrew-repo/releases/download/colorist-v0.1.21/"
-    cellar :any_skip_relocation
-    sha256 "a684ae81c634aba97be00b3ed563a6ab7f27b1843850f5f9ad63657b23dd6e90" => :catalina
   end
 
   test do
