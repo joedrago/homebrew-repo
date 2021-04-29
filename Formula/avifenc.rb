5a0d1dda69ce012c936829d6e91e43d8c555a3f1
Joe Drago <jdrago@netflix.com>
2021-04-28 17:00:14 -0700

    avifenc v0.9.0 (prebottle)

--- Formula/avifenc.rb
+++ Formula/avifenc.rb
@@ -1,12 +1,12 @@
 class Avifenc < Formula
   desc "avifenc / avifdec"
   homepage "https://github.com/AOMediaCodec/libavif"
-  url "https://github.com/AOMediaCodec/libavif/archive/v0.8.4.tar.gz"
-  sha256 "116b46a9a497c6ef178c5a20b912d8ac02f888dc8c537e40b5d16700823a9b16"
-  bottle do
-    root_url "https://github.com/joedrago/homebrew-repo/releases/download/avifenc-v0.8.4/"
-    sha256 cellar: :any_skip_relocation, catalina: "321f5bce8acd64c54ee29cbf7e30b4fd70707970bb6072681a5fc5ea23e12177"
-  end
+  url "https://github.com/AOMediaCodec/libavif/archive/v0.9.0.tar.gz"
+  sha256 "ea1603fc18e7dd20cf01f0b405156576886ecb5df84db8c0e87187cd2f8a00f4"
+  #bottle do
+  #  root_url "https://github.com/joedrago/homebrew-repo/releases/download/avifenc-v0.8.4/"
+  #  sha256 cellar: :any_skip_relocation, catalina: "321f5bce8acd64c54ee29cbf7e30b4fd70707970bb6072681a5fc5ea23e12177"
+  #end
 
   depends_on "cmake" => :build
   depends_on "nasm" => :build
